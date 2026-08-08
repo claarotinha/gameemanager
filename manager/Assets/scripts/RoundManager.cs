5bda32028f54637dd3ca07d90a8e7ecc9aa452ff
Natalia Gabriely <gabriely.j@escolar.ifrn.edu.br>
2026-08-08 08:43:21 -0300

    sistema de rounds

--- manager/Assets/scripts/RoundManager.cs
+++ manager/Assets/scripts/RoundManager.cs
@@ -1,17 +1,107 @@
 using UnityEngine;
+using TMPro;
 
 public class RoundManager : MonoBehaviour
 {
+    private int player1Wins = 0;
+    private int player2Wins = 0;
+
+    private TMP_Text player1RoundText;
+    private TMP_Text player2RoundText;
+
+    [Header("Players")]
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+
+    private Vector3 player1StartPosition;
+    private Vector3 player2StartPosition;
+
+    private void Start()
+    {
+        player1StartPosition = player1.position;
+        player2StartPosition = player2.position;
+
+        FindRoundUI();
+        UpdateRoundUI();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player1"))
         {
-            Debug.Log("Jogador 1 caiu!");
+            Player2WinsRound();
         }
 
         if (other.CompareTag("Player2"))
         {
-            Debug.Log("Jogador 2 caiu!");
+            Player1WinsRound();
         }
+    }
+
+    private void FindRoundUI()
+    {
+        player1RoundText = GameObject.Find("P1RoundText")?.GetComponent<TMP_Text>();
+        player2RoundText = GameObject.Find("P2RoundText")?.GetComponent<TMP_Text>();
+    }
+
+    private void UpdateRoundUI()
+    {
+        if (player1RoundText != null)
+        {
+            player1RoundText.text = "P1: " + player1Wins;
+        }
+
+        if (player2RoundText != null)
+        {
+            player2RoundText.text = "P2: " + player2Wins;
+        }
+    }
+
+    private void Player1WinsRound()
+    {
+        player1Wins++;
+
+        UpdateRoundUI();
+
+        Debug.Log("Player 1 ganhou o round!");
+        Debug.Log("Placar: P1 " + player1Wins + " x " + player2Wins + " P2");
+
+        CheckMatchWinner();
+    }
+
+    private void Player2WinsRound()
+    {
+        player2Wins++;
+
+        UpdateRoundUI();
+
+        Debug.Log("Player 2 ganhou o round!");
+        Debug.Log("Placar: P1 " + player1Wins + " x " + player2Wins + " P2");
+
+        CheckMatchWinner();
+    }
+
+    private void CheckMatchWinner()
+    {
+        if (player1Wins >= 2)
+        {
+            Debug.Log("PLAYER 1 GANHOU A PARTIDA!");
+        }
+        else if (player2Wins >= 2)
+        {
+            Debug.Log("PLAYER 2 GANHOU A PARTIDA!");
+        }
+        else
+        {
+            ResetRound();
+        }
+    }
+
+    private void ResetRound()
+    {
+        player1.position = player1StartPosition;
+        player2.position = player2StartPosition;
+
+        Debug.Log("Novo round começou!");
     }
 }
